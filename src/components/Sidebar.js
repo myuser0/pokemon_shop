3e3cd440ff13993e433aefcf2507430448666d7a
myuser0 <61796812+myuser0@users.noreply.github.com>
2020-07-05 15:08:45 -0300

    Add files via upload

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -61,58 +61,7 @@ export default function Sidebar({cartItems, setCartItems}) {
         { cartItems[0] ? <button id="proceed_checkout">Prosseguir para checkout</button> : ''}
       </div>
 
-
-      {/* <button
-        onClick={() => {
-          const text = prompt('Enter some text');
-          if (text) {
-            setCartItems(items => [
-              ...items,
-              { id: Math.floor(Math.random() * 1000), text },
-            ]);
-          }
-        }}
-      >
-        Add Item
-      </button> */}
-
     </aside>
-
-
-
-
-    // <aside className="sidebar_cart">
-    //   <div className="cart">
-    //     <h2>Carrinho</h2>
-    //     <ul>
-    //       <li>
-    //         <div className="cart_item_name">
-    //           <img src="#" alt=''/>
-    //           <p>Pikachu 1x</p>
-    //         </div>
-    //         <div className='cart_row_right'>
-    //           <p>$999</p>
-    //           <button>Remover</button>
-    //         </div>
-    //       </li>
-    //       <li>Charmander</li>
-    //       <li>Meow</li>
-    //       <li>Item 4</li>
-    //       <li>
-    //         <div className="cart_item_name">
-    //           <p>Total</p>
-    //         </div>
-    //         <div className='cart_row_right'>
-    //           <p>$999</p>
-    //         </div>
-    //       </li>
-    //       <li>Item 4</li>
-    //       <li>Item 4</li>
-    //     </ul>
-  
-    //     <button id="proceed_checkout">Prosseguir para checkout</button>
-    //   </div>
-    // </aside>
 
   )
 }
